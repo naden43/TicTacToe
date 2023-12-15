b9fc2d504dcdbe9e9020d6cb3f3b4352e9b953c5
Aser Eid <asereid56@gmail.com>
2023-12-15 23:16:06 +0200

    Add FX Computer-online-local mode

--- TicTacToeClient/src/tictactoeclient/Computer_modeController.java
+++ TicTacToeClient/src/tictactoeclient/Computer_modeController.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package tictactoeclient;
+package tic_tac_toe;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -16,7 +16,7 @@ import javafx.scene.control.Label;
  *
  * @author user
  */
-public class FXMLDocumentController implements Initializable {
+public class Computer_modeController implements Initializable {
     
     @FXML
     private Label label;
